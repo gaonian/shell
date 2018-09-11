94262c29656e404d21c7735014c0c98df4e35cb4
gaoyuh <gaoyuhang@kanzhun.com>
2018-09-11 17:57:29 +0800

    ..

--- test.sh
+++ test.sh
@@ -2,8 +2,34 @@
 
 #pgrep -l Google
 
-for temp in `pgrep -l Google`
-do
-  echo $temp
-  break
-done 
+# for temp in `pgrep -l Google`
+# do
+#   echo $temp
+#   break
+# done 
+
+# sed -i "" "s/fd/哈哈/g" file.txt
+
+echo `pwd`
+
+path="testName"
+[ -d "$path" ] || mkdir -p $path
+for i in `seq 10`; do
+	sudo touch $path/$i.txt
+done
+
+cd testName
+for i in `seq 10`; do
+	a="${i}.txt"
+	b="${i}.extension.TXT"
+	echo $a , $b
+	sudo mv $a $b
+done
+
+cd testName
+for i in `ls`; do
+	a=${i/.extension/}
+	sudo mv $i $a
+done
+
+
